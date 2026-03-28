e7a469bc95121a9a59476f6f251208c6fc72ec45
VictorLeung91 <victorleung91@gmail.com>
2026-03-27 22:06:58 -0600

    fix test for new UI

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders CICD text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/react cicd app/i);
+  expect(textElement).toBeInTheDocument();
 });
